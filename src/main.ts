cb16de921bc52277c870cd09c16ba49e9546b2a8
qinmenghuan <qinmenghuan@ovfintech.com>
2019-09-10 19:41:34 +0800

    配置vant组件

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入vant
+import 'vant/lib/index.css';
+import { Button } from 'vant';
+Vue.use(Button);
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+
+        <van-button type="primary">主要按钮</van-button>
+    </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class About extends Vue {}
+</script>
